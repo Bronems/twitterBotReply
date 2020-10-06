209b90b46f2c2d6937b39a3c8b37d2cdb57366cd
Bronems <k.galus@icloud.com>
2020-10-06 14:37:59 +0200

    first commit

---
+++ kill.js
@@ -0,0 +1,21 @@
+const config = require('./conf.js');
+const Twit = require('twit');
+var T = new Twit(config);
+
+T.get('statuses/user_timeline', {screen_name: 'Klemens_devacc',count: 100,include_rts: true}, del);
+function del(err, data){
+    var i = 0;
+    if(err)
+        console.log(err);
+    if(data.length < 1)
+        console.log("NULL");
+    else if(data){
+        console.log(data.length);
+        for(i = 0 ; i < data.length ; i++){
+            idDest = data[i].id_str;
+            T.post('statuses/destroy/:id', { id: idDest }, function (err, data, response) { 
+            });
+        } 
+        console.log("Deleted " + i + " tweet !");
+    }
+}
---
+++ main.js
@@ -0,0 +1,24 @@
+const config = require('./conf.js');
+const Twit = require('twit');
+var T = new Twit(config);
+var stream = T.stream('statuses/filter', {track: '@Klemens_devacc'});
+
+function desc(err,data){
+    if(err)
+        console.log(err);
+}
+    
+stream.on('tweet', mentioned);
+
+function mentioned(tweet){
+    var message = tweet.text;
+    var de = tweet.user.screen_name;
+    var deNom = tweet.user.name;
+    var tweetId = tweet.id_str;
+    var stat = ("@" +de +" Hello !");
+
+    T.post('favorites/create', {id: tweetId}, desc);
+
+    T.post('statuses/update', { status : stat , in_reply_to_status_id: tweetId}, desc);
+}
+
