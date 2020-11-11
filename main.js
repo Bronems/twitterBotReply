8b40cdaf2c3dfbaf394f0512564af4d8193a15ac
Bronems <k.galus@icloud.com>
2020-11-11 23:27:11 +0000

    a

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ var stream = T.stream('statuses/filter', {track: '@Klemens_devacc'});
 function desc(err,data){
     if(err)
         console.log(err);
+    console.log(data);
 }
     
 stream.on('tweet', mentioned);
@@ -17,7 +18,16 @@ function mentioned(tweet){
     var deNom = tweet.user.name;
     var tweetId = tweet.id_str;
     var stat = ("@" +de +" Hello !");
+    var string = message.replace(de, '');
+    var string = string.toLowerCase();
 
+    console.log(message);
+    if(string.includes("sang"))
+        var stat = ("@" +de +" Wesh ma gueule !");
+    if(string.includes("de la merde")){
+        var stat = ("T'as raison @" +de +" c'est de la merde !");
+    }
+    
     T.post('favorites/create', {id: tweetId}, desc);
 
     T.post('statuses/update', { status : stat , in_reply_to_status_id: tweetId}, desc);
